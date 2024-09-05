1a135ae6b919fb96af2bb903311b5bbad0acda44
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-05 12:28:18 +0800

    Hari Keempat

---
+++ Day4.java
@@ -0,0 +1,6 @@
+public class Day4 {
+    
+    public static void main(String[] args){
+        System.out.printf("Nama = %s, Nim = %s, Umur =%d, Tinggi Badan = %f ", "Abraham Deppadian", "D0224001", 17 , 160.8);
+    }
+}
