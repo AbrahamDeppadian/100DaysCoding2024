d1b9d29fd56945ae373bb8c06e37ef0fccd01079
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-07 15:59:46 +0800

    Hari keenam

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		String nama = ("Abraham Deppadian");
+		int umur = 17;
+		
+		System.out.println (nama);
+		System.out.println (umur);
+	}
+}
