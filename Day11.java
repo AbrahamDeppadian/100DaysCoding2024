5d744c2ca206f385d4cc51dd8d634e3628265715
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-12 17:09:13 +0800

    Hari ke-sebelas

---
+++ Day11.java
@@ -0,0 +1,8 @@
+public class Day11 {
+	public static void main(String[] args) {
+		/*short hampir mirip dengan byte,short mencakup dari -32.768 sampai 32.767*/
+		short no = 32555;
+		System.out.println (no);
+	
+	}
+}
