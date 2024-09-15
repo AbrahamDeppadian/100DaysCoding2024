75d526f23746845e8e39f3767b25cc3406afced5
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-15 20:01:24 +0800

    hari ke Empat Belas

---
+++ Day14.java
@@ -0,0 +1,11 @@
+public class Day14 {
+	public static void main(String[] args) {
+		//long bisa mencakup angka sampai 9,2 kuadriliun
+
+		long value =9223372036854775807L;
+		long Min_value = -9223372036854775808L; 
+
+		System.out.println (value);
+		System.out.println (Min_value);
+	}
+}
