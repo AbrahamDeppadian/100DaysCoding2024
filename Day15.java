1116de25aa14122097ee7e3013f6ebf0ea062fad
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-16 21:04:00 +0800

    Hari ke-15

---
+++ Day15.java
@@ -0,0 +1,11 @@
+public class Day15 {
+	public static void main(String[] args) {
+		//double untuk angka desimal
+		
+		double a =1.7;
+		double b =1.8;
+		double c = a+b;
+		
+		System.out.println(c);
+		}
+}
