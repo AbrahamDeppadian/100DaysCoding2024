e4f11e917a66f305d49a3a1ab23b4edc55308792
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-17 20:32:17 +0800

    Hari ke-16

---
+++ Day16.java
@@ -0,0 +1,8 @@
+public class Day16 {
+	public static void main(String[] args) {
+		//float untuk angka desimal namun hanya beberapa angka di belakang koma,dan juga value harus diakhiri dengan huruf F agar java mnbacanya sebagai float
+		
+		float koma = 7.97998F;
+		System.out.println(koma);
+		}
+}
