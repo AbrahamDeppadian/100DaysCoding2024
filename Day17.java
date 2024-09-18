766f794c7aadd61e88237f914d3007a77abcb2fd
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-18 21:02:11 +0800

    Hari ke-17

---
+++ Day17.java
@@ -0,0 +1,14 @@
+public class Day17 {
+	public static void main(String[] args) {
+		
+		//Char untuk memasukkan value hanya satu karakter
+		
+		char inisial = 'A';
+		char angka = '1';
+		char simbol = '#';
+		
+		System.out.println ("inisial:" +inisial);
+		System.out.println("angka:"+angka);
+		System.out.println ("simbol:" +simbol);
+		}
+}
