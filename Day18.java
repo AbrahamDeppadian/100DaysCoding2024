c121abd8ab5cfa8edec5d44018dfa2709ad3084c
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-19 21:18:19 +0800

    Hari ke-18

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class Day18{
+	public static void main(String[] args) {
+		
+		//tipe data boolean digunakan untuk memeriksa apakah pernyataan kita benar atau tidak
+		
+		String a = "ayam";
+		String b = "babi";
+		
+		
+		
+		boolean c = a==b ;
+		
+		System.out.println(a + "sama jenisnya dengan");
+		System.out.println (b);
+		System.out.println(c);
+		}
+}
