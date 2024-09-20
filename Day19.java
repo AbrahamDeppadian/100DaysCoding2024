46f39bee89078be95ec01dd091a50543a3bd5a32
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-20 19:41:13 +0800

    Hari ke-19

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Day19{
+	public static void main(String[] args) {
+		
+		//string mencakup kata,kalimat,angka,huruf,dan lain-lain namun harus menggunakan petik dua
+		String kata = "masa depan";
+		
+		
+		System.out.println(kata);
+	
+		}
+}
