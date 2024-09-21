93201c747ab7e9ef40f26830efcb8039a826f2dd
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-21 22:22:01 +0800

    Hari ke-20

---
+++ Day20.java
@@ -0,0 +1,9 @@
+public class Day20 {
+	public static void main(String[] args) {
+		//tipe data refference Byte sama dengan tipe data primitif bye namun penulisannya harus diawali dengan huruf kapital
+		
+		Byte angka = 10;
+		System.out.println(angka);
+	
+	} 
+}
