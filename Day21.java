081e9b4887a494eab632fe02d05469e9224d72a0
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-22 20:45:40 +0800

    Hari ke-21

---
+++ Day21.java
@@ -0,0 +1,9 @@
+public class Day21 {
+	public static void main(String[] args) {
+		//tipe data refference Short sama dengan tipe data primitif short namun penulisannya harus diawali dengan huruf kapital
+		
+		Short nilai = 123;
+		System.out.println(niali);
+	
+	} 
+}
