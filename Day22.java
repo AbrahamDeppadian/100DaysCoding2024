9ed800a9bf3f0378d8207ef308853671a7a0ca19
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-23 08:48:02 +0800

    Hari ke-22

---
+++ Day22.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+	//tipe data refference Integer sama dengan tipe data primitif Integer namun penulisannya harus diawali dengan huruf kapital
+		
+		Integer a= 123;
+		System.out.println (a); 
+	}
+}
