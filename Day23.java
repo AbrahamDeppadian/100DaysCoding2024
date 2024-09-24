28f9cd0d29067d5e28bb58ae2c59085a5b5dd95e
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-24 19:01:18 +0800

    Hari ke -23

---
+++ Day23.java
@@ -0,0 +1,13 @@
+public class Day23 {
+	public static void main(String[] args) {
+		 
+		 Long min =Long.MIN_VALUE;
+		 Long max =Long.MAX_VALUE;
+		 Long $= 123456789101112L;
+		 
+		 
+		 System.out.println("Nilai terkecil dari Long: " + min);
+		 System.out.println("Nilai terbesar dari Long: " + max);
+		 System.out.println("angka: "+ $);
+	}
+}
