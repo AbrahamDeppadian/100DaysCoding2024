6558481f11aab89044e3d3d5b223ecdfd15de857
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-09-25 17:02:02 +0800

    Hari ke-24

---
+++ Day24.java
@@ -0,0 +1,9 @@
+public class Day24 {
+	public static void main(String[] args) {
+		//tipe data refferences Float sama dengan tipe data primitif float namun penulisannya diawali dengan huruf capital
+		
+		Float A= 132.7F;
+		System.out.println(A);
+		
+	}
+}
