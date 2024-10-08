ffd94f0dcdc07b9e16a041f608eeb272e1252914
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-10-08 18:16:35 +0800

    Hari ke-37

---
+++ Day 37.java
@@ -0,0 +1,10 @@
+public class Abraham{
+	public static void main(String[]Args){
+	int $ = 710;
+    //penambahan dan pengupdatean nilai 
+    $ += 13;
+     //pengurangan dan pengupdatean nilai
+     $ -=12;
+     System.out.println($); 
+	}
+}
