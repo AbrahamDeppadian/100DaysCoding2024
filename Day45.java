eac2d5c40b02363836d5491ab2dfdb0ad6932abb
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-10-16 18:30:06 +0800

    Hari ke-45

---
+++ Day45.java
@@ -0,0 +1,10 @@
+public class AbrahamD {
+    
+    public static void main(String[] args){
+        int A = 7;
+        int D = 11;
+        System.out.println(A<D||A==D);
+        
+    }
+    
+}
