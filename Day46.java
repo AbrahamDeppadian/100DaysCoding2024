8006348519ff1657771fea0ce134294d61923d64
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-10-17 22:09:48 +0800

    Hari ke-46

---
+++ Day46.java
@@ -0,0 +1,12 @@
+public class main{
+        public static void main(String[]Args){
+        
+            int a = 100;
+            int b = 10;
+            
+            System.out.println(a!=b);
+         
+                
+    }
+    
+}
