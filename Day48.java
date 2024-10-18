f1c87f5a5c7eef3c328cc951f5f4af5e4eeec311
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-10-19 01:21:21 +0800

    Hari ke-48

---
+++ Day48.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main{
+        public static void main(String[]Args){
+        Scanner input = new Scanner (System.in);
+         System.out.println("masukkan angka: ");
+         int a = input.nextInt();
+         
+         if (a>0){
+                System.out.println("bilangan positif");
+             }else{
+                 System.out.println("bilangan negatif atau nol");
+             }
+         }
+    
+}
