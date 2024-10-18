17abf94c92ded6916e86a2e2f298849e15934be0
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-10-18 20:30:49 +0800

    Hari ke-47

---
+++ Day47.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+        public static void main(String[]Args){
+        Scanner input = new Scanner (System.in);
+         System.out.println("masukkan angka: ");
+         int a = input.nextInt();
+         
+         if (a>0){
+                System.out.println("bilangan positif");
+             }
+         }
+    
+}
