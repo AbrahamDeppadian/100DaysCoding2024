3ede4facfcbe4e0ffc687ba56b87c37c8744e3f1
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-11-03 20:48:22 +0800

    Hari ke-63

---
+++ Day63.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String[]args){
+        for (int a = 0; a<=10; a++){
+           if (i==5){
+                    continue; 
+           }
+           System.out.println(a);
+      
+        }
+    }
+}
