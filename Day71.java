7203bb2b2afed1d70120e248a734ef504b2a85bb
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-11-11 22:37:43 +0800

    Hari ke-71

---
+++ Day71.java
@@ -0,0 +1,10 @@
+public class Abraham {
+
+    public static void salam() {
+        System.out.println("Halo, saya Abraham!");
+    }
+
+    public static void main(String[] args) {
+        Abraham.salam();
+    }
+}
