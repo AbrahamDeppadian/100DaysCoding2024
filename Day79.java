dd0a50e59eafc3f11c52ac49101f5771790fcd6e
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-11-19 22:23:29 +0800

    Hari ke-79

---
+++ Day79.java
@@ -0,0 +1,24 @@
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String jurusan;
+
+    public Mahasiswa(String nama, String nim, String jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+
+    @Override
+    public String toString() {
+        return "Mahasiswa{" +
+                "Nama='" + nama + '\'' +
+                ", NIM='" + nim + '\'' +
+                ", Jurusan='" + jurusan + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa("Andi", "123456", "Teknik Informatika");
+        System.out
+          
