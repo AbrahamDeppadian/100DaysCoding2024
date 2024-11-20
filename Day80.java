d695ca08317bfff105eb7e9773b3808b24d42227
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-11-20 23:42:56 +0800

    Hari ke-80

---
+++ Day80.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[]args){
+        String a = "Abrahammmmmmm";
+        String potong = s.trim();
+        System.out.println("Kata asli:"+s);
+        System.out.println("Kata yang sudah di ubah:" + potong  );
+    }
+}
