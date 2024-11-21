edcd00e438c130c94aebe8f82a35ebbccbfe238c
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-11-21 20:55:57 +0800

    Hari ke-81

--- Day81.java
+++ Day81.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        int x=1000;
+        double x=98.7;
         String y=String.valueOf(x);
         System.out.println(y);
     }
