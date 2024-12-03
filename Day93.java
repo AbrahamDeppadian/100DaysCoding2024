a527c04f7ee4247f35ef056766f1e5187aee415b
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-12-03 23:20:39 +0800

    Hari ke-93

--- Day93.java
+++ Day93.java
@@ -1,16 +1,14 @@
+
 public class Main {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            if (i != n - 1) {
-                for (int j = 0; j < (n - i - 1) * 2 - 1; j++) {
+            for (int j = 0; j < n * 2 - 1; j++) {
+                if (j == n - 1 - i || j == n - 1 + i) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
-                System.out.print("*");
             }
             System.out.println();
         }
