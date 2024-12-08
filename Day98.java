973138c31096dccd4b0e9a466d27bb82fa8f7b9c
AbrahamDeppadian <abrahammamasa97@gmail.com>
2024-12-08 20:02:32 +0800

    Hari ke-98

--- Day98.java
+++ Day98.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
       Scanner input=new Scanner (System.in);
-      System.out.println("Masukkan nilai a: ");
-        int a= input.nextInt();
-      System.out.println("Masukkan nilai b: ");
-        int b = input.nextInt();
+      System.out.println("Masukkan nilai x: ");
+        int x= input.nextInt();
+      System.out.println("Masukkan nilai y: ");
+        int y = input.nextInt();
         int total = 0;
 
-        if (a % 2 != 0) {
+        if (x % 2 != 0) {
         }
 
-        for (int i = a; i <= b; i += 2) {
+        for (int i = x; i <= y; i += 2) {
             total += i;
         }
-        System.out.println("Total penjumlahan bilangan genap dari " + a + " ke " + b + " adalah: " + total);
+        System.out.println("Total penjumlahan bilangan genap dari " + x + " ke " + y + " adalah: " + total);
     }
 }
